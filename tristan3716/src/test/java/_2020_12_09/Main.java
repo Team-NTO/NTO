09594deea1207c891207ea8cf9e9e00cfd1cac33
Hyeon-uk <39885326+tristan3716@users.noreply.github.com>
2021-03-14 13:40:23 +0900

    2021/03/14 백준 20210 파일 탐색기

--- tristan3716/src/test/java/_2020_12_09/Main.java
+++ tristan3716/src/test/java/_2020_12_09/Main.java
@@ -6,137 +6,140 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    private static class NaturalOrderComparator implements Comparator<String> {
-        private static char order(char x) {
-            if (x == 0) {
-                return 0;
-            }
-            char ch;
-            if ('A' <= x && x <= 'Z') {
-                ch = (char) (x * 2);
-            } else {
-                ch = (char) ((x - 32) * 2 + 1);
-            }
-            return ch;
-        }
-        int compareRight(String a, String b) {
-            int bias = 0, ia = 0, ib = 0;
+    private static class Word implements Comparable<Word> {
+        List<String> list;
 
-            for (; ; ia++, ib++) {
-                char ca = charAt(a, ia);
-                char cb = charAt(b, ib);
-
-                if (isDigit(ca) && isDigit(cb)) {
-                    return bias;
-                }
-                if (isDigit(ca)) {
-                    return -1;
-                }
-                if (isDigit(cb)) {
-                    return +1;
-                }
-                if (ca == 0 && cb == 0) {
-                    return bias;
-                }
-
-                if (bias == 0) {
-                    if (ca < cb) {
-                        bias = -1;
-                    } else if (ca > cb) {
-                        bias = +1;
+        public Word(String str) {
+            boolean isNumber = false;
+            this.list = new ArrayList<>();
+            StringBuilder subWord = new StringBuilder();
+            for (char x : str.toCharArray()) {
+                if (Character.isDigit(x)) {
+                    if (!isNumber && subWord.length() > 0) {
+                        list.add(subWord.toString());
+                        subWord = new StringBuilder();
                     }
+                    subWord.append(x);
+                    isNumber = true;
+                } else {
+                    if (isNumber && subWord.length() > 0) {
+                        list.add(subWord.toString());
+                        subWord = new StringBuilder();
+                    }
+                    subWord.append(x);
+                    isNumber = false;
                 }
+            }
+            if (subWord.length() > 0) {
+                list.add(subWord.toString());
             }
         }
 
         @Override
-        public int compare(String a, String b) {
-            int ia = 0, ib = 0;
-            int nza, nzb;
-            char ca, cb;
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            for (String x : this.list) {
+                sb.append(x);
+            }
+            return sb.toString();
+        }
 
-            while (true) {
-                nza = nzb = 0;
+        @Override
+        public int compareTo(Word other) {
+            int length = Math.min(this.list.size(), other.list.size());
+            for (int i = 0; i < length; i++) {
+                int t = compare(this.list.get(i), other.list.get(i));
+                if (t != 0) {
+                    return t;
+                }
+            }
+            return this.list.size() - other.list.size();
+        }
 
-                ca = charAt(a, ia);
-                cb = charAt(b, ib);
+        private int compare(String a, String b) {
+            char[] arr = a.toCharArray();
+            char[] brr = b.toCharArray();
+            if (Character.isDigit(arr[0])) {
+                if (Character.isDigit(brr[0])) { // 00
+                    int ac = 0;
+                    int bc = 0;
+                    for (int i = 0; i < arr.length; i++) {
+                        if (arr[i] != '0') {
+                            break;
+                        }
+                        ac++;
+                    }
+                    for (int i = 0; i < brr.length; i++) {
+                        if (brr[i] != '0') {
+                            break;
+                        }
+                        bc++;
+                    }
+                    String aa = a.substring(ac);
+                    String bb = b.substring(bc);
 
-                while (Character.isSpaceChar(ca) || ca == '0') {
-                    if (ca == '0') {
-                        nza++;
-                    } else {
-                        nza = 0;
+                    if (aa.length() < bb.length()) {
+                        return -1;
+                    } else if (aa.length() > bb.length()) {
+                        return 1;
                     }
 
-                    ca = charAt(a, ++ia);
-                }
-
-                while (Character.isSpaceChar(cb) || cb == '0') {
-                    if (cb == '0') {
-                        nzb++;
-                    } else {
-                        nzb = 0;
+                    int c = aa.compareTo(bb);
+                    if (c == 0) {
+                        return ac - bc;
                     }
 
-                    cb = charAt(b, ++ib);
-                }
-
-                if (Character.isDigit(ca) && Character.isDigit(cb)) {
-                    int bias = compareRight(a.substring(ia), b.substring(ib));
-                    if (bias != 0) {
-                        return bias;
-                    }
-                }
-                if (ca == 0 && cb == 0) {
-                    return compareEqual(a, b, nza, nzb);
-                }
-                ca = order(ca);
-                cb = order(cb);
-                if (ca < cb) {
+                    return c;
+                } else { // 0A
                     return -1;
                 }
-                if (ca > cb) {
-                    return +1;
+            } else {
+                if (Character.isDigit(brr[0])) { // A0
+                    return 1;
+                } else { // AA
+                    int length = Math.min(arr.length, brr.length);
+                    for (int i = 0; i < length; i++) {
+                        int ca = arr[i];
+                        int cb = brr[i];
+                        if (ca >= 'a') {
+                            ca -= 'a';
+                            ca = ca * 2 + 1;
+                        } else {
+                            ca -= 'A';
+                            ca = ca * 2;
+                        }
+                        if (cb >= 'a') {
+                            cb -= 'a';
+                            cb = cb * 2 + 1;
+                        } else {
+                            cb -= 'A';
+                            cb = cb * 2;
+                        }
+
+                        if (ca < cb) return -1;
+                        else if (ca > cb) return 1;
+                    }
                 }
-
-                ++ia;
-                ++ib;
             }
-        }
 
-        static boolean isDigit(char c) {
-            return !Character.isDigit(c) && c != '.' && c != ',';
-        }
-
-        static char charAt(String s, int i) {
-            return i >= s.length() ? 0 : s.charAt(i);
-        }
-
-        static int compareEqual(String a, String b, int nza, int nzb) {
-            if (nza - nzb != 0)
-                return nza - nzb;
-
-            if (a.length() == b.length())
-                return a.compareTo(b);
-
-            return a.length() - b.length();
+            return Integer.compare(a.length(), b.length());
         }
     }
 
     public static void main(String[] args) throws IOException {
-        ArrayList<String> list = new ArrayList<>();
+        List<Word> list = new ArrayList<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         for (int i = 0; i < n; i++) {
-            list.add(br.readLine());
+            list.add(new Word(br.readLine()));
         }
 
-        list.sort(new NaturalOrderComparator());
-
+        list.sort(null);
         StringBuilder sb = new StringBuilder();
-        for (String x : list) {
-            sb.append(x).append('\n');
+        for (Word x : list) {
+            sb.append(x.toString()).append('\n');
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
     }
 }
